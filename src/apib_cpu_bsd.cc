f3c803e546f76c8615cb78adc8f56e1ecf329a5f
Gregory Brail <gregbrail@google.com>
2019-10-16 22:31:49 -0700

    Small BSD fixes.

--- src/apib_cpu_bsd.cc
+++ src/apib_cpu_bsd.cc
@@ -14,9 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+#include <sys/types.h>
 #include <sys/sysctl.h>
 #include <sys/times.h>
-#include <sys/types.h>
 #include <unistd.h>
 
 #include <cstring>
